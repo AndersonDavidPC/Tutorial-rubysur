76dba26d4d36a94aace522c77e058dc0d87bfdc1
Anderson David Patiño Caicedo <andersondavidpc@ufps.edu.co>
2023-01-04 22:10:53 -0500

    Textos

---
+++ 2.Textos/textos.rb
@@ -0,0 +1,21 @@
+# puts "Hola, mundo!"
+# puts ""
+# puts "Adiós."
+
+# Aritmetica de cadenas
+# puts "Me gusta " + "el pastel de manzana."
+# puts "Me gusta" + " el pastel de manzana."
+
+#Multiplicación
+# puts "parpadeo"*4
+
+#Stings vs números
+puts  12  +  12
+puts "12" + "12"
+puts "12  +  12"
+
+puts  2  *  5
+puts "2" *  5
+puts "2  *  5"
+
+puts "¡Mi sobrenombre es 'Pepe'!"
