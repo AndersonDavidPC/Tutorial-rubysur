7437d238348fce3bcde51bff88ac6ce4eabdf8f5
Anderson David Patiño Caicedo <andersondavidpc@ufps.edu.co>
2023-01-04 22:11:07 -0500

    Variables

---
+++ 3.Variables/variables.rb
@@ -0,0 +1,34 @@
+# myString='...puedes decir eso de nuevo...'
+# puts myString
+# puts myString
+
+# name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+# puts 'Me llamo ' + name + '.'
+# puts 'Wow!  "' + name + '" es un nombre realmente largo!'
+
+# Reasignación
+# composer = 'Mozart'
+# puts composer + ' fue "el amo", en su día.'
+
+# composer = 'Beethoven'
+# puts 'Pero yo prefiero a ' + composer + ', personalmente.'
+
+# Reasignando tipo de objeto
+# var = 'solo otro ' + 'texto'
+# puts var
+
+# var = 5 * (1+2)
+# puts var
+
+
+# Una variable apuntando a otra
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'ocho'
+puts var1
+puts var2
